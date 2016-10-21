c6a61f28d7e69e25f21d4ebd830771c54082e4eb
John Zhu <366036746@qq.com>
2016-10-22 01:34:28 +0800

    tally(积分区)

---
+++ scripts/focus.js
@@ -0,0 +1,13 @@
+/**
+ * @author: John Zhu
+ * @description:
+ * @Date: 2016/10/20 14:44
+ */
+window.onload = function () {
+    var picBox = document.getElementsByClassName()emenById('picBox');
+    var pic = document.getElementsByClassName('pic');
+    var bottons = document.getElementsByClassName('bottons').ElementsByTagName('span');
+
+
+
+}
